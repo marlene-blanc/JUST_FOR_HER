13ebbdf93346d9b80d929d36e961c14ef42678c8
Fanny <venezia.fanny@gmail.com>
2022-11-30 18:32:53 +0100

    geocoding et autocomplete form,  sans le front

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -10,12 +10,23 @@ class DonationsController < ApplicationController
     @donation = Donation.new
     @donation.products = Product.find(params["products"].keys.map(&:to_i))
     @donation.user = current_user
-    raise
 
     if @donation.save!
-    redirect_to ##
+      redirect_to ##
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def select_warehouse
+    @warehouses = Warehouse.all
+    @markers = @warehouses.geocoded.map do |warehouse|
+      {
+        lat: warehouse.latitude,
+        lng: warehouse.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { warehouse: warehouse }),
+        image_url: helpers.asset_url("logo.png")
+      }
     end
   end
 end
--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,4 +1,16 @@
 class WarehousesController < ApplicationController
   def create
   end
+
+  def select_warehouse
+    @warehouses = Warehouse.all
+    @markers = @warehouses.geocoded.map do |warehouse|
+      {
+        lat: warehouse.latitude,
+        lng: warehouse.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { warehouse: warehouse }),
+        image_url: helpers.asset_url("logo.png")
+      }
+    end
+  end
 end
