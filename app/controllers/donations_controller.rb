34bc8c5050c0b16abcab733ea2c23c262b8734fe
marlene blanc <blanc.marlene@gmail.com>
2022-12-01 12:08:40 +0100

    Ajout home,product,select_warehouse

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -8,14 +8,34 @@ class DonationsController < ApplicationController
 
   def create
     @donation = Donation.new
-    @donation.products = Product.find(params["products"].keys.map(&:to_i))
+    @donation.products = Product.find(params["products"].values.map(&:to_i))
     @donation.user = current_user
-    raise
 
     if @donation.save!
-    redirect_to ##
+      redirect_to donation_select_warehouse_path(@donation)
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def select_warehouse
+    @donation = Donation.find(params[:donation_id])
+    @warehouses = Warehouse.all
+  end
+
+  def update_warehouse
+    @warehouse = Warehouse.new(warehouse_params)
+    @warehouse.update(params[:donation_id])
+
+  end
+
+  def list
+    @products = Product.all
+  end
 end
+
+private
+
+  def warehouse_params
+    params.require(:warehouse).permit(:user_id, :address)
+  end
