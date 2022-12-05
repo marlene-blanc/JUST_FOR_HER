9f3d77dca658e8ee97031da4db5292b0b8c846a1
asma chouar <asmachouar@hotmail.fr>
2022-12-05 18:10:05 +0100

    frontpagefinancedonation

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :donations, only: %i[new create show] do
+  resources :donations, only: %i[new create show]
+  get "/financial_show", to: "donations#financial_show" do
     get "/deposit_option", to: "donations#deposit_option"
-
     get "/select_warehouse", to: "donations#select_warehouse"
     patch "/select_warehouse", to: "donations#update_warehouse"
     patch "/confirm", to: "donations#confirm"
   end
-  #get "select_warehouse", to: "donations#select_warehouse"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   get "/thank_you1", to: "donations#thank_you1"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
-# Donations_Controller
-# def select_warehouse
-#    @warehouse =  Warehouse.new
-# end
 
 # simple_form_for @warehouse
 # button => méthode create du controller Warehouse, redirect_to => donation/:id/confirm
