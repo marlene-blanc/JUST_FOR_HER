ae105b71890fd0850db05cde1d851c59fc288bbf
asma chouar <asmachouar@hotmail.fr>
2022-12-06 14:33:38 +0100

    frontpageprices

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :donations, only: %i[new create show]
-  get "/financial_show", to: "donations#financial_show" do
+  resources :donations, only: %i[new create show] do
     get "/deposit_option", to: "donations#deposit_option"
     get "/select_warehouse", to: "donations#select_warehouse"
     patch "/select_warehouse", to: "donations#update_warehouse"
     patch "/confirm", to: "donations#confirm"
   end
-
+  get "/financial_show", to: "donations#financial_show"
   get "/thank_you1", to: "donations#thank_you1"
 end
 
